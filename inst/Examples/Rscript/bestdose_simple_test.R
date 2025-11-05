d98617721b76c119def3e389973c76f962aeebba
Julián D. Otálvaro <juliandavid347@gmail.com>
2025-11-05 19:28:31 +0000

    missing files

---
+++ inst/Examples/Rscript/bestdose_simple_test.R
@@ -0,0 +1,37 @@
+library(Pmetrics)
+
+setwd("inst/Examples/Runs")
+
+mod_onecomp <- PM_model$new(
+    pri = list(
+        ke = ab(0.001, 3.0),
+        v = ab(25.0, 250.0)
+    ),
+    eqn = function() {
+        dx[1] <- -ke * X[1] + B[1]
+    },
+    out = function() {
+        Y[1] <- X[1] / v
+    },
+    err = list(
+        additive(1, c(0, 0.20, 0, 0))
+    )
+)
+
+
+past_file <- "../src/bestdose_past.csv"
+target_file <- "../src/bestdose_target.csv"
+prior_file <- "../src/bestdose_prior.csv"
+
+
+bd <- PM_bestdose$new(
+    prior = prior_file,
+    model = mod_onecomp,
+    past_data = past_file,
+    target = target_file,
+    dose_range = list(min = 0, max = 300),
+    bias_weight = 0.0,
+    target_type = "concentration"
+)
+
+bd$print()
